dfdbcedd7842a622a73bf8b9c319dda15e6f6e6b
MARK-PC\markk <markkpamy@gmail.com>
2017-05-04 10:44:04 +0200

    no message

--- ClientTFTP/src/clienttftp/Client.java
+++ ClientTFTP/src/clienttftp/Client.java
@@ -5,7 +5,11 @@
  */
 package clienttftp;
 
+import java.io.FileOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
+
 
 /**
  *
@@ -13,17 +17,43 @@ import java.net.InetAddress;
  */
 public class Client {
     
+private DatagramSocket socket;
+private byte sentBuffer[];
+private byte receiveBuffer[];	
+private byte[] donneesFichier;
+private int serverPort = 69;
+private InetAddress serverAddress;
+    
 public Client(){
 
 }
 
-public int receiveFile(InetAddress addr, String nomFichierDistant,String nomFichierLocal){
+public int receiveFile(String addr,int port, String nomFichierDistant,String nomFichierLocal) throws Exception{
+    
+    DatagramPacket RRQ;
+    DatagramPacket DATA;
+    DatagramPacket ACK;
+    FileOutputStream file;
+    
+    DatagramSocket portCom = new DatagramSocket();
+     serverAddress = InetAddress.getByName(addr);
     return 0;
 
 }
 
-public int sendFile(InetAddress addr,String nomFichierLocal){
+public int sendFile(InetAddress addr,int port,String nomFichierLocal){
+    
+    
+    return 0;
 
+}
+
+public byte[] paquetWRQ(){
+    return null;
+
+}
+public byte[] paquetRRQ(){
+    return null;
 }
     
 }
