2682a5250ece05dda48be54822b736af6b95b908
MARK-PC\markk <markkpamy@gmail.com>
2017-05-04 09:11:11 +0200

    no message

---
+++ ClientTFTP/src/clienttftp/Client.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienttftp;
+
+/**
+ *
+ * @author markk
+ */
+public class Client {
+    
+public Client(){
+
+}
+
+public sendFile(InetAddress addr, String )
+    
+}
