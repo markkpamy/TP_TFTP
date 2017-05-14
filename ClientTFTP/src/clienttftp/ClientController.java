54f64c9d374631a3c2d8c1f405ea6ef085540f80
Fabien Ogli <fabien.ogli@gmail.com>
2017-05-14 17:46:37 +0200

    Ajout Controller

---
+++ ClientTFTP/src/clienttftp/ClientController.java
@@ -0,0 +1,43 @@
+package clienttftp;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+/**
+ * Created by Fabien on 14/05/2017.
+ */
+public class ClientController {
+
+    @FXML
+    private Button send;
+
+    @FXML
+    private TextField adress;
+
+    @FXML
+    private TextField port;
+
+    private Client client;
+
+    public ClientController() {
+    }
+
+    @FXML
+    private void initialize()
+    {
+
+    }
+
+    @FXML
+    private void send(){
+        client = new Client();
+        String adr = adress.getText();
+        String po = port.getText();
+
+        System.out.println(adress.getText()+" "+port.getText());
+    }
+
+}
--- ClientTFTP/src/clienttftp/ClientTFTP.java
+++ ClientTFTP/src/clienttftp/ClientTFTP.java
@@ -8,37 +8,30 @@ package clienttftp;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  *
  * @author markk
  */
 public class ClientTFTP extends Application {
+
+    private Stage primaryStage;
+    private Pane pane;
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+
+        initWindow();
     }
 
     /**
@@ -46,7 +39,25 @@ public class ClientTFTP extends Application {
      */
     public static void main(String[] args) {
         launch(args);
-        Client client = new Client();
+        Client client = new Client("127.0.0.1");
+    }
+
+    public void initWindow()
+    {
+        try{
+            //Chargement du fichier fxml
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(ClientTFTP.class.getResource("View.fxml"));
+            pane = loader.load();
+
+            //Affiche sur l application
+            Scene scene = new Scene(pane);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
     
 }
